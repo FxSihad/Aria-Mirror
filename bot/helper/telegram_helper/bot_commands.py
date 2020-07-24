eb011758e8db611d03c24aec2d6ff1b279fc2076
BianSepang <bianconneri46@gmail.com>
2020-07-25 05:00:55 +0800

    changed bot commands *mirror to gd *unpackmirror to up *authorize to auth *unauthorize to unauth *clone to cl

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,21 +1,21 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
-        self.UnzipMirrorCommand = 'unzipmirror'
+        self.MirrorCommand = 'gd'
+        self.UnzipMirrorCommand = 'up'
         self.TarMirrorCommand = 'tarmirror'
         self.CancelMirror = 'cancel'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
         self.StatusCommand = 'status'
-        self.AuthorizeCommand = 'authorize'
-        self.UnAuthorizeCommand = 'unauthorize'
+        self.AuthorizeCommand = 'auth'
+        self.UnAuthorizeCommand = 'unauth'
         self.PingCommand = 'ping'
         self.RestartCommand = 'restart'
         self.StatsCommand = 'stats'
         self.HelpCommand = 'help'
         self.LogCommand = 'log'
-        self.CloneCommand = "clone"
+        self.CloneCommand = "cl"
         self.WatchCommand = 'watch'
         self.TarWatchCommand = 'tarwatch'
 
